e19d3d6eab73d2ddf1781c59023a2c3a8dff1a6e
dmiore <dimao@mail.ru>
2015-12-13 00:18:40 +0300

    Added parce ldap data func.

--- ldap_user.py
+++ ldap_user.py
@@ -4,6 +4,7 @@
 from ConfigParser import SafeConfigParser
 from sys import exit
 import logging
+import ldap
 
 # define env variables
 logging_file = 'log'
@@ -14,12 +15,15 @@ conf_files = ['public.conf', 'mac_info.conf']
 config = SafeConfigParser()
 config.read(conf_files)
 conf_warn = ""
+exclude_login = []
 
 try:
-    logging_file = config.get('logging', 'file1')
+    exclude_conf_login = config.get('logins', 'exclude')
+    exclude_login = exclude_conf_login.split(",")
+    logging_file = config.get('logging', 'file')
     logging_conf_level = config.get('logging', 'level')
 except Exception, e:
-    conf_warn = "Can't read logging config:" + str(e)
+    conf_warn = "Can't read config: " + str(e)
     logging_conf_level = None
 
 # init log file
@@ -59,8 +63,44 @@ except Exception, e:
     logging.critical("Can't read config: " + str(e))
     exit(1)
 
-print ad_ldap_url, ad_ldap_bind_dn, ad_ldap_secret, ad_ldap_base_dn, mysql_host, mysql_user, mysql_passwd, mysql_db
+
+def get_ldap_users():
+    res = []
+    try:
+        ad = ldap.initialize(ad_ldap_url)
+        ad.simple_bind_s(ad_ldap_bind_dn, ad_ldap_secret)
+        filterexp = 'objectClass=user'
+        scope = ldap.SCOPE_SUBTREE
+        attrlist = ['mail', 'sAMAccountName']
+        res = ad.search_s(ad_ldap_base_dn, scope, filterexp, attrlist)
+        ad.unbind_s()
+    except Exception, err:
+        logging.error("Can't read ldap data: " + str(err))
+    return res
+
+
+def parce_ldap(data):
+    res = {}
+    if len(data) == 0:
+        return res
+    for line in data:
+        data_user_groups = line[0][3:-len(ad_ldap_base_dn)-1].split(',', 1)
+        user = data_user_groups[0]
+        group = ""
+        mail = ""
+        login = ""
+        if len(data_user_groups) == 2:
+            group = data_user_groups[1].replace("OU=","")
+        if 'mail' in line[1]:
+            mail = line[1]['mail'][0]
+        if 'sAMAccountName' in line[1]:
+            login = line[1]['sAMAccountName'][0]
+        if login in exclude_login:
+            continue
+        print login, user, mail, group
 
 
 if __name__ == "__main__":
-    print "ii"
+    ldap_data = get_ldap_users()
+    user_dict = parce_ldap(ldap_data)
+
